d4c0f4e03ac58160e4455442274b1469c4900f6e
albertofcasuso <casuso@tutanota.com>
2019-11-19 19:09:21 -0600

    Added ES Lint part 3 Finished

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ app.put('/api/persons/:id',(request,response,next)=>{
 })
 
 const errorHandler = (error,request,response,next) => {
-  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {    
     return response.status(400).json({ error: error.message })  }
